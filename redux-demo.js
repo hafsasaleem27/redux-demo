45a84bf977c302b97c6e04a0cf5e7ef7c11c32cc
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-25 23:28:16 +0500

    dispatching a decrement action

--- redux-demo.js
+++ redux-demo.js
@@ -2,18 +2,27 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
 
 const storeSubscribe = () => {
-    const latestState = store.getState();
-    console.log(latestState);
+  const latestState = store.getState();
+  console.log(latestState);
 };
 
 store.subscribe(storeSubscribe);
 
 store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
